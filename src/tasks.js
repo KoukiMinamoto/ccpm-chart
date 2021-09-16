7e7c3d5b043ecf8ab67f7d2a6d30840483c6b201
tomato <tomatomusicstudio@gmail.com>
2021-09-16 17:07:30 +0900

    test

--- src/tasks.js
+++ src/tasks.js
@@ -2,7 +2,6 @@ function createSticker () {
     const widget = [{
         "type": "sticker",
         "title": "Enter Task",
-        "scale": 4.0,
         "style": {
           "backgroundColor": "#ff00ff"
         }
