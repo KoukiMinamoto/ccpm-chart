d154615aa4d5d13722e2809828d100bada952d5c
tomato <tomatomusicstudio@gmail.com>
2021-09-16 16:21:44 +0900

    aaaa

--- src/tasks.js
+++ src/tasks.js
@@ -1,11 +1,11 @@
 function createSticker () {
-    const widget = {
+    const widget = [{
         "type": "card",
         "title": "developer card",
         "style": {
           "backgroundColor": "#ff00ff"
         }
-      }
+      }]
 
       miro.board.widgets.create(widget)
 }
