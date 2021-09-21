6bceba52ff1bd8ba3df27d4ef889904f262f45c2
tomato <tomatomusicstudio@gmail.com>
2021-09-21 13:03:22 +0900

    change position

--- src/tasks.js
+++ src/tasks.js
@@ -2,6 +2,8 @@ function createSticker () {
     const widget = [{
         "type": "sticker",
         "title": "Enter Task",
+        "y": 0.0,
+        "x": 0.0,
         "style": {
           "backgroundColor": "#ff00ff"
         }
